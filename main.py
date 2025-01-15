efc0f69350762663110dbb30cf32d7c4fa7757f6
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-01-15 04:39:04 -0600

    Getting messages in batch. Still limited. Will work on reauthenticating every time, getting nextPae, ratelimiting and 100 batch limit

--- main.py
+++ main.py
@@ -18,29 +18,39 @@ profile = requests.get(
     headers=headers,
 )
 
-messages = requests.get(f"https://gmail.googleapis.com/gmail/v1/users/dadaabdulhafiz0306%40gmail.com/messages?key={key}",
-                        headers=headers)
+messages = requests.get(f"https://gmail.googleapis.com/gmail/v1/users/dadaabdulhafiz0306%40gmail.com/messages?q=after:2024/06/01 before:2025/01/14&key={key}&maxResults=500",
+                        headers=headers) #get all messages within a time frame. could add sender here to by adding with f string from:({sender_emails}) which is a string of emails with OR separating each.
+print(messages.json())
+# for message in messages.json():
+ 
+messageIds = [messageDict["id"] for messageDict in messages.json()["messages"]]
+with open("messageIdList.json", "w") as file:
+    json.dump(messageIds, file, indent=4)
 
-for message in messages.json()["messages"][0:20]:
-    message = requests.get(f"https://gmail.googleapis.com/gmail/v1/users/dadaabdulhafiz0306%40gmail.com/messages/{message['id']}?format=full&key={key}",
-                        headers=headers)
-    # with open(f"{count}.json", "w") as file:
-    #     json.dump(message.json(), file, indent=10)
-    # print(message.json()["payload"]["body"]["data"])
+
+
+# for message in messages.json()["messages"][0:20]:
+#     message = requests.get(f"https://gmail.googleapis.com/gmail/v1/users/dadaabdulhafiz0306%40gmail.com/messages/{message['id']}?format=full&key={key}",
+#                         headers=headers)
+#     # with open(f"{count}.json", "w") as file:
+#     #     json.dump(message.json(), file, indent=10)
+#     # print(message.json()["payload"]["body"]["data"])
     
-    encoded_body = message.json().get("payload", {}).get("body", {}).get("data", "")
+#     encoded_body = message.json().get("payload", {}).get("body", {}).get("data", "")
 
-    # Decode the Base64 data
-    decoded_bytes = base64.urlsafe_b64decode(encoded_body)
-    decoded_content = decoded_bytes.decode("utf-8")
+#     # Decode the Base64 data
+#     decoded_bytes = base64.urlsafe_b64decode(encoded_body)
+#     decoded_content = decoded_bytes.decode("utf-8")
 
-    # Check if the content is HTML
-    mime_type = message.json().get("payload", {}).get("mimeType", "")
-    if mime_type == "text/html":
-        # Parse the HTML content
-        soup = BeautifulSoup(decoded_content, "html.parser")
-        readable_content = soup.get_text()
-        print("HTML Content (Readable):")
-        print(readable_content)
+#     # Check if the content is HTML
+#     mime_type = message.json().get("payload", {}).get("mimeType", "")
+#     if mime_type == "text/html":
+#         # Parse the HTML content
+#         soup = BeautifulSoup(decoded_content, "html.parser")
+#         readable_content = soup.get_text()
+#         print("HTML Content (Readable):")
+#         print(readable_content)
     
-    print("\n")
+#     print("\n")
+    
+# https://www.googleapis.com/gmail/v1/users/me/messages?q=in:sent after:2014/01/01 before:2014/02/01
